f3a6ac3cd13a92411e7b29417f9d66da57d019a1
David Oh <david.oh@blackline.com>
2019-07-25 13:26:11 -0700

    Clean and refactor global event bus

--- components/Receiver.vue
+++ components/Receiver.vue
@@ -9,17 +9,10 @@ import { eventBus } from '~/plugins/eventBus.js'
 
 export default {
   name: 'receiver',
-  // mounted() {
-  //   //   this.combinedData
-  //   eventBus.$on('combinedData', Data => {
-  //     console.log("Hello World");
-  //   });
-  // },
   created() {
     eventBus.$on('jsonData', (json) => {
       this.combinedData = json;
     });
-    // console.log(this.combinedData);
   },
   mounted() {
     eventBus.$on('jsonData', (json) => {
--- components/StatusPanel.vue
+++ components/StatusPanel.vue
@@ -10,88 +10,80 @@ export default {
   name: "status-panel",
   components: {
   },
-  data() {
-    return {
-      jsonData: eventBus.$emit('test-event', this.combinedData)
-    }
-  },
-  asyncData() {
-    // 'http://localhost:3000/release.json',
-    // 'http://localhost:3000/responsetime.json',
-    // 'http://localhost:3000/status.json',
-    // 'http://localhost:3000/transaction.json'
-    
-    // url+releaseJSON,
-    // url+responsetimeJSON,
-    // url+statusJSON,
-    // url+transactionJSON
-  },
-  props: [
-    'combinedData'
-  ],
   methods: {
-    switchName() {
-      return this.myName;
-    },
     emitData() {
       this.$emit("combinedData", this.combinedData);
     }
   },
   mounted() {
     const localurl = 'http://localhost:3000/';
-    var releaseJSON = 'release.json';
-    var responsetimeJSON = 'responsetime.json';
-    var statusJSON = 'status.json';
-    var transactionJSON = 'transaction.json';
+    var releaseJSON_URL = 'release.json';
+    var responsetimeJSON_URL = 'responsetime.json';
+    var statusJSON_URL = 'status.json';
+    var transactionJSON_URL = 'transaction.json';
+    var resourceJSON_URL = 'resource.json';
+    var fetchDate = new Date();
 
-    var releaseJSONRequest = 
-    fetch(localurl+releaseJSON)
+    var releaseJSON = 
+    fetch(localurl+releaseJSON_URL)
     .then(function(response) {
       return response.json()
     });
 
-  var responseTimeJSONRequest = 
-  fetch(localurl+responsetimeJSON)
-    .then(function(response) {
-      return response.json()
-    });
+    var responseTimeJSON = 
+    fetch(localurl+responsetimeJSON_URL)
+      .then(function(response) {
+        return response.json()
+      });
 
-  var statusJSONRequest = 
-  fetch(localurl+statusJSON)
-    .then(function(response) {
-      return response.json()
-    });
+    var statusJSON = 
+    fetch(localurl+statusJSON_URL)
+      .then(function(response) {
+        return response.json()
+      });
 
-  var transactionJSONRequest = 
-  fetch(localurl+transactionJSON)
-    .then(function(response) {
-      return response.json()
-    });
-  
-  var combinedData = {
-    releaseJSONRequest: {},
-    responseTimeJSONRequest: {},
-    statusJSONRequest: {},
-    transactionJSONRequest: {}
-    };
-  Promise.all([
-    releaseJSONRequest,
-    responseTimeJSONRequest,
-    statusJSONRequest,
-    transactionJSONRequest])
+    var transactionJSON = 
+    fetch(localurl+transactionJSON_URL)
+      .then(function(response) {
+        return response.json()
+      });
+
+    var resourceJSON = 
+    fetch(localurl+resourceJSON_URL)
+      .then(function(response) {
+        return response.json()
+      });
+    
+    var date = fetchDate;
+
+    var combinedData = {
+      releaseJSON: {},
+      responseTimeJSON: {},
+      statusJSON: {},
+      transactionJSON: {},
+      resourceJSON: {},
+      date: {}
+      };
+    Promise.all([
+      releaseJSON,
+      responseTimeJSON,
+      statusJSON,
+      transactionJSON,
+      resourceJSON,
+      date
+      ])
     .then(function(data) {
-      combinedData["releaseJSONRequest"] = data[0]["release"];
-      combinedData["responseTimeJSONRequest"] = data[1]["metrics"];
-      combinedData["statusJSONRequest"] = data[2];
-      combinedData["statusJSONRequest"] = data[2]["environments"];
-      combinedData["statusJSONRequest"] = data[2]["incidents"]
-      combinedData["transactionJSONRequest"] = data[3]["metrics"];
+      combinedData["releaseJSON"] = data[0]["release"];
+      combinedData["responseTimeJSON"] = data[1]["metrics"];
+      combinedData["statusJSON"] = data[2];
+      combinedData["statusJSON"] = data[2]["environments"];
+      combinedData["statusJSON"] = data[2]["incidents"]
+      combinedData["transactionJSON"] = data[3]["metrics"];
+      combinedData["resourceJSON"] = data[4];
+      combinedData["date"] = data[5];
       eventBus.$emit('jsonData', JSON.stringify(combinedData));
       return JSON.stringify(combinedData);
     });
-    // console.log(combinedData)
-
-  // this.$bus.$emit('test-event', combinedData);
   }
 }
 </script>
